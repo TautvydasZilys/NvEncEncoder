503212c2181735c38bbf54be8ae43548c2fd41df
Sunius <suniusx@gmail.com>
2015-06-07 19:35:44 +0300

    Actually initialize NvAPI functions.

--- NvEncEncoder/Main.cpp
+++ NvEncEncoder/Main.cpp
@@ -1,6 +1,41 @@
 #include "PrecompiledHeader.h"
 
+HMODULE g_NvEncodeApiDll;
+NV_ENCODE_API_FUNCTION_LIST g_NvAPI;
+
+inline static void InitializeNvApiFunctions()
+{
+#if _WIN64
+	const wchar_t nvEncodeApiDllName[] = L"nvEncodeAPI64.dll";
+#else
+	const wchar_t nvEncodeApiDllName[] = L"nvEncodeAPI.dll";
+#endif
+
+	g_NvEncodeApiDll = LoadLibraryW(nvEncodeApiDllName);
+	Assert(g_NvEncodeApiDll != nullptr);
+
+	typedef NVENCSTATUS(NVENCAPI* NvEncodeAPICreateInstanceFunc)(NV_ENCODE_API_FUNCTION_LIST *functionList);
+	auto nvEncodeAPICreateInstance = reinterpret_cast<NvEncodeAPICreateInstanceFunc>(GetProcAddress(g_NvEncodeApiDll, "NvEncodeAPICreateInstance"));
+	Assert(nvEncodeAPICreateInstance != nullptr);
+
+	g_NvAPI.version = NV_ENCODE_API_FUNCTION_LIST_VER;
+
+	auto nvstatus = nvEncodeAPICreateInstance(&g_NvAPI);
+	Assert(nvstatus == NV_ENC_SUCCESS);
+}
+
+inline static void CleanupNvApiFunctions()
+{
+	ZeroStructure(&g_NvAPI);
+	
+	auto result = FreeLibrary(g_NvEncodeApiDll);
+	Assert(result != FALSE);
+}
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
+	InitializeNvApiFunctions();
+
+	CleanupNvApiFunctions();
 	return 0;
 }
--- NvEncEncoder/PrecompiledHeader.h
+++ NvEncEncoder/PrecompiledHeader.h
@@ -3,3 +3,15 @@
 #include <Windows.h>
 
 #include "External\nvEncodeAPI.h"
+
+#if _DEBUG
+#define Assert(x) do { if (!(x)) { if (IsDebuggerPresent()) { __debugbreak(); } } } while (false)
+#else
+#define Assert(x)
+#endif
+
+template <typename T>
+inline void ZeroStructure(T* structure)
+{
+	ZeroMemory(structure, sizeof(T));
+}
